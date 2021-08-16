d96764f4b186e25e5b305962a6a40de6dfdc81e1
Phaneendrabejawada <phaneendra622@msitprogram.net>
2021-08-16 15:03:41 +0530

    done

--- 05-getinrange-Python/getinrange.py
+++ 05-getinrange-Python/getinrange.py
@@ -6,4 +6,14 @@
 
 def fun_getinrange(x, bound1, bound2):
 	# your code goes here
-	return -1
+	if bound1<x<bound2 or bound2<x<bound1:
+		return x
+	if bound1<bound2:
+		if x<bound1:
+			return bound1
+		return bound2
+	if bound1>bound2:
+		if x<bound2:
+			return bound2
+		return bound1
+	
