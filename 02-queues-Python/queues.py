044f87b173d13a6180bd3a2a5a44c9f695170c7b
Phaneendrabejawada <phaneendra622@msitprogram.net>
2021-08-16 15:31:02 +0530

    done

--- 02-queues-Python/queues.py
+++ 02-queues-Python/queues.py
@@ -4,15 +4,42 @@ you'd like! Try to write each one in as
 few lines as possible.
 Make sure you pass the test cases too!"""
 
-class Queue:
+class Element(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList(object):
     def __init__(self, head=None):
-        self.storage = [head]
+        self.head = head
+        
+    def append(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
 
-    def enqueue(self, new_element):
-        pass
+    def insert_first(self, new_element):
+        new_element.next = self.head
+        self.head = new_element
 
-    def peek(self):
-        pass 
+    def delete_first(self):
+        deleted = self.head
+        if self.head:
+            self.head = self.head.next
+            deleted.next = None
+        return deleted
 
-    def dequeue(self):
-        pass
+class stack(object):
+    def __init__(self,top=None):
+        self.ll = LinkedList(top)
+
+    def push(self, new_element):
+        self.ll.insert_first(new_element)
+
+
+    def pop(self):
+        return self.ll.delete_first()
