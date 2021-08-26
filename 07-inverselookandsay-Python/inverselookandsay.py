074d664544a57c989838cd9929e45f6ecfe266b2
Phaneendrabejawada <phaneendra622@msitprogram.net>
2021-08-26 19:32:53 +0530

    done

--- 07-inverselookandsay-Python/inverselookandsay.py
+++ 07-inverselookandsay-Python/inverselookandsay.py
@@ -22,4 +22,14 @@
 
 def inverselookandsay(a):
 	# Your code goes here
-	pass
+	x = []
+	count = 0
+	n = 0
+	if len(a[0]) == 0:
+		return x
+	for i in range(len(a)):
+		count = (a[i])[0]
+		n = (a[i])[1]
+		for c in range(count):
+			x.append(n)
+	return x
